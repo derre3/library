242dae72d75aed01c96594306715ad05b33066da
Andre <123985988+derre3@users.noreply.github.com>
2023-07-06 17:25:58 -0300

    add function to display book cards onscreen

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// placeholder books
+let myLib = [
+    new Book("The Lightning Thief", "Rick Riordan", 384, false),
+    new Book("The Hunger Games", "Suzanne Collins", 486, true),
+    new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 236, false),
+];
+
 function addBook() {
     let newBook = new Book(title, author, pages, read);
-    myLib.push(newBook)
+    myLib.push(newBook);
+    displayCards();
     return `${newBook.title} added to library`
 }
 
@@ -19,8 +27,37 @@ function getBook() {
     return addBook();
 }
 
-let myLib = [
-    new Book("The Lightning Thief", "Rick Riordan", 384, false),
-    new Book("The Hunger Games", "Suzanne Collins", 486, true),
-    new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 236, false),
-];
+function displayCards() {
+    const cardContainer = document.querySelector('.card-container')
+    let i = 0;
+    myLib.forEach(book => {
+        i++;
+        const cardItem = document.createElement('div')
+        cardItem.className = 'card'
+        cardContainer.appendChild(cardItem)
+        
+        const titleCard = document.createElement('p');
+        const authorCard = document.createElement('p');
+        const pagesCard = document.createElement('p');
+        const readLabel = document.createElement('label');
+        const readCard = document.createElement('input');
+        readCard.type = 'checkbox';
+        readCard.name = `read${i}`;
+        readCard.id = `read${i}`;
+        readLabel.htmlFor = `read${i}`;
+
+        titleCard.textContent = book.title;
+        authorCard.textContent = book.author;
+        pagesCard.textContent = `${book.pages} Pages`;
+        readCard.checked = book.read;
+        readLabel.textContent = 'Read?';
+
+        cardItem.appendChild(titleCard);
+        cardItem.appendChild(authorCard);
+        cardItem.appendChild(pagesCard);
+        cardItem.appendChild(readLabel);
+        cardItem.appendChild(readCard);
+    });
+}
+
+displayCards();
